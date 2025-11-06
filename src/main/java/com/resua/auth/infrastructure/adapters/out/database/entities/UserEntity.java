c94c8850fed5a60f4bd2dd824147cb5a9421f7b6
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-11-06 04:20:54 -0500

    Creado adaptador de salida de base de datos UserEntity

---
+++ src/main/java/com/resua/auth/infrastructure/adapters/out/database/entities/UserEntity.java
@@ -0,0 +1,46 @@
+package com.resua.auth.infrastructure.adapters.out.database.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Table(name = "users")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "profession")
+    private String profession;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "security_question")
+    private String securityQuestion;
+
+    @Column(name = "secret_answer")
+    private String secretAnswer;
+}
