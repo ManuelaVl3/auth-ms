260d15b8886ff876c49d66ecdce0f8804ae46365
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-11-06 04:40:00 -0500

    Creado puerto de salida UserRepository y añadida query de búsqueda por credenciales

--- src/main/java/com/resua/auth/infrastructure/ports/out/database/UserRepository.java
+++ src/main/java/com/resua/auth/infrastructure/ports/out/database/UserRepository.java
@@ -1,4 +1,18 @@
 package com.resua.auth.infrastructure.ports.out.database;
 
-public interface UserRepository {
+import com.resua.auth.infrastructure.adapters.out.database.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    @Query("SELECT u FROM UserEntity u " +
+            "WHERE u.email = :email AND u.password = :password")
+    Optional<UserEntity> findUserByCredentials(@Param("email") String email, @Param("password") String password);
+
 }
