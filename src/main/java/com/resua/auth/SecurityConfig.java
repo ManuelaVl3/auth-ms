26355e62f330b17b99394be498e6c833bc28244b
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-11-12 23:33:37 -0500

    Añadido PaswordEncoder y SecurityChainFilter con excepciones para login y registro de usurio

--- src/main/java/com/resua/auth/SecurityConfig.java
+++ src/main/java/com/resua/auth/SecurityConfig.java
@@ -1,4 +1,36 @@
 package com.resua.auth;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(authz -> authz
+//              Si se quiere ver el swagger, descomentar las siguientes líneas
+//                        .requestMatchers("/swagger-ui.html").permitAll()
+//                        .requestMatchers("/swagger-ui/**").permitAll()
+//                        .requestMatchers("/api-docs/**").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/user").permitAll()
+
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
 }
