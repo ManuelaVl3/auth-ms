7167087d43e8902f1c27515abc9a6014921587a2
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-11-15 21:53:45 -0500

    Actualizado LoginUserImpl para el manejo de JWT

--- src/main/java/com/resua/auth/application/usecases/LoginUserImpl.java
+++ src/main/java/com/resua/auth/application/usecases/LoginUserImpl.java
@@ -2,10 +2,17 @@ package com.resua.auth.application.usecases;
 
 import com.resua.auth.domain.models.User;
 import com.resua.auth.infrastructure.adapters.in.request.LoginRequestDTO;
+import com.resua.auth.infrastructure.adapters.in.response.LoginResponseDTO;
 import com.resua.auth.infrastructure.adapters.out.database.UserAdapter;
 import com.resua.auth.infrastructure.ports.in.LoginUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import com.resua.auth.infrastructure.services.jwt.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
@@ -13,13 +20,46 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class LoginUserImpl implements LoginUser {
 
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
     private final UserAdapter userAdapter;
 
     @Override
-    public Optional<User> login(LoginRequestDTO loginRequest) {
-        return userAdapter.getUserByCredentials(
-                loginRequest.getEmail(),
-                loginRequest.getPassword()
+    public LoginResponseDTO login(LoginRequestDTO loginRequest) throws AuthenticationException {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
+
+        String token = jwtService.generateToken(userDetails);
+
+        // Se hizo necesario este manejo para poder obtener los datos que se necesitan en el endpoint /login
+        Optional<User> user = userAdapter.getUserByCredentials(loginRequest.getEmail(), loginRequest.getPassword());
+
+        Long id = 0L;
+        String name = "";
+        String lastName = "";
+        String email = "";
+
+        if(user.isPresent()){
+            id = user.get().getId();
+            name = user.get().getName();
+            lastName = user.get().getLastName();
+            email = user.get().getEmail();
+        }
+
+        return new LoginResponseDTO(
+                "Login exitoso",
+                id,
+                name + " " + lastName,
+                email,
+                true,
+                token
         );
     }
 }
