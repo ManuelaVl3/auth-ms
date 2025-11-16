43e09c3e60fed8dab7435baef5f3e951c9868882
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-11-15 21:50:39 -0500

    Modificada interface LoginUser. Ahora login es del tipo LoginResponseDTO

--- src/main/java/com/resua/auth/infrastructure/ports/in/LoginUser.java
+++ src/main/java/com/resua/auth/infrastructure/ports/in/LoginUser.java
@@ -1,12 +1,11 @@
 package com.resua.auth.infrastructure.ports.in;
 
-import com.resua.auth.domain.models.User;
 import com.resua.auth.infrastructure.adapters.in.request.LoginRequestDTO;
+import com.resua.auth.infrastructure.adapters.in.response.LoginResponseDTO;
 
-import java.util.Optional;
 
 public interface LoginUser {
 
-    Optional<User> login(LoginRequestDTO loginRequest);
+    LoginResponseDTO login(LoginRequestDTO loginRequest);
 }
 
